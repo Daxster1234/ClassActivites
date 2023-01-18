7f9513f8070cca6ce0fc349dafe94bb7bfbc90bf
Dax Meredith <dax.meredith@gmail.com>
2023-01-17 22:37:35 -0500

    Section 2 up to 2.2.2

---
+++ Section02/2.1/practice.md
@@ -0,0 +1,17 @@
+# Header
+## Second Header
+### Third Header
+_Italics_
+
+**Bold**
+
+~~strikethrough~~
+- thing 1
+- thing 2
+- thing 3
+
+1. one
+1. two
+1. three
+
+
---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+
+
+// create a constant variable (const)
+const myName = "Dax";
+
+// create a variable that can be reassigned (let)
+let age = 21;
+// create three variables and assign them values of different data types (=)
+const var1 = "string";
+const var2 = 21;
+let changeMe = true;
+
+// print the types of two variables that reference two different data types (typeof)
+console.log(typeof var1);
+console.log(typeof var2);
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+changeMe = 2324;
+
+// print the type of the variable you just changed (typeof)
+console.log(typeof changeMe);
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+const name = "John";
+const lang = "JavaScript";
+let lenWks = 2;
+let master = false;
+
+console.log("Hello, my name is " + name + ", and I am learning " + lang + ". I have been practicing for only " +  lenWks + " weeks, so my status as a master is: " + master + ".");
+
+
